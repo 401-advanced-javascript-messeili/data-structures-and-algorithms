b1047adebb224d99b8cd716d72012a04160242d6
messeili <mymoa.2014@gmail.com>
2020-11-17 02:03:48 +0200

    fix function

--- challenges/mergeSort/merge-sort.js
+++ challenges/mergeSort/merge-sort.js
@@ -50,37 +50,5 @@ function merge(left, right, arr) {
   }
 }
 
-// function merge(left, right, arr1) {
-//   let i = 0;
-//   let j = 0;
-//   let k = 0;
-//   while (i < left.length && j < right.length) {
-//     // console.log('inside');
-//     if (left[i] <= right[j]) {
-//       arr1[k] = left[i];
-//       i = i + 1;
-//       // console.log('arr1',arr1);
-//     } else {
-//       arr1[k] = right[j];
-//       // console.log('else arr1',arr1);
-//       j = j + 1;
-//     }
-//     k = k + 1;
-//   }
-//   if (i === left.length) {
-//     while (j < right.length) {
-//       arr1[k] = right[j];
-//       j = j + 1;
-//       k = k + 1;
-//     }
-//   } else {
-//     while (i < left.length) {
-//       arr1[k] = left[i];
-//       i = i + 1;
-//       k = k + 1;
-//     }
-//   }
-// }
-
 let y = mergeSort([5, 9, 8, 7, 2, 1, 6]);
 console.log(y);
